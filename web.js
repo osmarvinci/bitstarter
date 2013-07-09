7d1755c4fcd9504a90017550a1f75ad7715dea3a
Osmar Vinci <osmarvinci@gmail.com>
2013-07-09 22:17:15 +0000

    Adicionado web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+//Get info form index.html
+var home = fs.readFileSync("index.html", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(home);
 });
 
 var port = process.env.PORT || 5000;
