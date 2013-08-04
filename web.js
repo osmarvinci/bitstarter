00251b39c753416f802431e844514ba1673bebed
osmarvinci <osmarvinci@gmail.com>
2013-08-04 02:12:45 +0000

    aa

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 //Get info form index.html
-var home = fs.readFileSync("teste2.html", "utf-8");
+var home = fs.readFileSync("index.html", "utf-8");
 
 app.get('/', function(request, response) {
   response.send(home);
