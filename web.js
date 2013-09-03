8badd59da1ded4cfb89a4524c38d209c6d6ac75e
osmarvinci <osmarvinci@gmail.com>
2013-09-02 23:46:50 -0300

    added update email version

--- web.js
+++ web.js
@@ -1,15 +1,29 @@
-var express = require('express');
-var fs = require('fs');
-var app = express.createServer(express.logger());
-//Get info form index.html
-var home = fs.readFileSync("index.html", "utf-8");
+var async   = require('async')
+  , express = require('express')
+  , fs      = require('fs')
+  , http    = require('http')
+  , https   = require('https')
+  , db      = require('./models');
 
-app.get('/', function(request, response) {
-  response.send(home);
-});
-
+var app = express();
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+app.get('/', function(request, response) {
+  var data = fs.readFileSync('index.html').toString();
+  response.send(data);
+
+});
+
+//URL e-mail
+app.post('/email',function(request,response){
+
+console.log(request.body.email);
+fs.appendFile('contacts.txt', request.body.email + '\n', function (err) {
+});
+response.send(request.body.email);
+
 });
